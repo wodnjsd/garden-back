f076d454733c0927f60fc1aa72ec3680bb738695
roshanak <roshanak.ahmed11@gmail.com>
2022-06-18 22:19:25 +0600

    plant data  shortened paragraphs

--- db/data/data.js
+++ db/data/data.js
@@ -151,7 +151,7 @@ export default [
 
 },
 {
-  name: "asd",
+  name: "",
   commonName: "Pachira Aquatica",
   about: "There's real charm to this houseplant, with its umbrella-like growth and beautiful plaited trunk. It's pretty easy to care for, too. All it asks is a fairly bright spot in your home and an occasional drink. \n The money tree is known by many lovely names: French peanut, Guiana nut, Malabar chestnut, provision tree, monguba, pumpo. Or to botanists, pachira aquatica. It's usually found growing in swamps in Central and South America. It's plaited trunk doesn't occur naturally, but it's become traditional to plait it when grown as a houseplant. For reasons that aren't entirely clear, this plant is believed to bring good fortune and prosperity. In Asia, it's become very popular as an office plant, as it's thought it will help a business thrive. Got to be worth a try, right? \n This plant does well in both home or office. It just likes to live somewhere light, though out of direct sun, and to be watered when the top two inches of soil feel dry. It also likes to be misted occasionally. Look after it and in return it might make you rich, hopefully.",
   likes: "light watering, most light conditions",
@@ -161,12 +161,3 @@ export default [
 }
 
 ]
-
-{
-  name: '',
-  commonName: "Pachira Aquatica",
-  about: "There’s real charm to this houseplant, with its umbrella-like growth and beautiful plaited trunk. It’s pretty easy to care for, too. All it asks is a fairly bright spot in your home and an occasional drink.",
-  size: "medium",
-  image: "",
-  price: 60,
-}

e32afd8f045a29d49fb373f9ee0aa56b7ff8d0a2
roshanak <roshanak.ahmed11@gmail.com>
2022-06-22 02:01:56 +0600

    plants.js update

--- models/plants.js
+++ models/plants.js
@@ -13,7 +13,6 @@ const plantSchema = new mongoose.Schema({
   name: { type: String, required: false },
   commonName: { type: String, required: true },
   about: { type: String, required: true },
-  commonName: { type: String, required: true },
   likes: { type: String, required: true },
   size: { type: String, required: true },
   image: { type: String, required: false },

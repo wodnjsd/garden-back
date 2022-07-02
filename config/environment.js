026ab1e540d0b274fbd05751f457e7a1f17b57d1
Jaewon Han <wodnjsd@gmail.com>
2022-07-02 15:46:24 +0100

    dburi changed

--- config/environment.js
+++ config/environment.js
@@ -2,14 +2,14 @@
 import dotenv from 'dotenv'
 dotenv.config()
 // export const dbURL = 'mongodb://127.0.0.1:27017/plantsdb'
-export const mongoURL = 'mongodb://127.0.0.1:27017'
+export const dbURI = process.env.DB_URI || 'mongodb://127.0.0.1:27017/plantsdb'
 
 // ! If mocha is running your tests, line 7 will be true, and so the
 // ! DB URL will be pokemondb-test. If you're running express normally
 // ! it will be pokemondb
-export const dbURI = process.env.DB_URI || mongoURL === 'test' ?
-  `${mongoURL}/plantsdb-test` : 
-  `${mongoURL}/plantsdb`
+// export const dbURI = process.env.DB_URI || mongoURL === 'test' ?
+//   `${mongoURL}/plantsdb-test` : 
+//   `${mongoURL}/plantsdb`
 export const port = process.env.PORT || 4000
 
 // ! Create a secret for JWT verification
